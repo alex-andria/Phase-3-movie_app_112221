535c58c213d7412cb9cdc723da3e446a9c79b197
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-01-10 16:01:48 -0500

    commit tree ca2da201eaf01e21b1385210d02690e2929f43b2

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,22 @@
 def initialize_app 
     puts 'welcome to phase-3'
+
+    movies = ['Interstellar', "Spiderman", "Titanic"]
+
+    puts "Welcome to Flatiron Movies"
+    puts "Please choose an option:"
+    puts "1. List all Movies"
+    puts "0. to exit"
+
+    user_input = gets.strip
+
+    if user_input==1
+        movies.each do | movie | puts movie
+        binding.pry
+        end
+    else
+        puts "Goodbye :)"
+    end
 end 
 
 
